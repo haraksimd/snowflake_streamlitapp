1370a2ac38de607aeac0bf7cce0317c14e69b9f9
haraksimd <79120336+haraksimd@users.noreply.github.com>
2023-06-07 10:53:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Snowflake project")
+streamlit.header("Breakfast")
+streamlit.text("🥣 Oatmeal")
+streamlit.text("🥗 Smoothie")
+streamlit.text("🐔 Egg")
+streamlit.text("🥑🍞 Avocado toast")
