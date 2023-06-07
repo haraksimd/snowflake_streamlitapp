ece764b49a7058a44e75ea4b73f7db50546858cb
haraksimd <79120336+haraksimd@users.noreply.github.com>
2023-06-07 11:13:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 
 fruit_list_df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list_df = fruit_list_df.set_index('Fruit')
 
 
 streamlit.title("Snowflake project")
